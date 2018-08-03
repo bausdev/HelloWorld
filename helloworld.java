18d7ea5dd9230173caf54bedbce2310a5892decf
Ben Fry <19857726+bausdev@users.noreply.github.com>
2018-08-03 01:11:43 -0500

    Update helloworld.java
    I needed to remove some lines of code that were not necessary

--- helloworld.java
+++ helloworld.java
@@ -3,17 +3,6 @@ public class Hello {
 
         System.out.println("Hello, Ben!");
 
-        // Creating variables
-
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
-
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-
-        int myDifference = 1000 - myTotal;
-
-        System.out.println(myDifference);
     }
 }
 
